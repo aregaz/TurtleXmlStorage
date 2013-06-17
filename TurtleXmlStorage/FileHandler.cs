1d02d9259773aa028679c51f8c6385bb61937656
aregaz-job <illia.ratkevych@gmail.com>
2013-06-17 19:50:00 +0300

    Basic architecture added.
    Couple [empty] classes added.

---
+++ TurtleXmlStorage/FileHandler.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace TurtleXmlStorage
+{
+    public class FileHandler
+    {
+        public string FileName { get; set; }
+        public string FilePath { get; set; }
+
+        public FileHandler(string fileName)
+        {
+            this.FileName = fileName;
+        }
+
+        public string ReadFile()
+        {
+            if (!this.IsFileExist())
+            {
+                this.CreateFile();
+            }
+
+            var fileContent = new StringBuilder();
+            var stream = File.OpenText(this.FilePath);
+            var contentLine = string.Empty;
+            while ((contentLine = stream.ReadLine()) != null)
+            {
+                fileContent.Append(contentLine);
+            }
+
+            return fileContent.ToString();
+        }
+
+        public void CreateFile()
+        {
+            File.Create(this.FilePath);
+        }
+
+        public bool IsFileExist()
+        {
+            return File.Exists(this.FilePath);
+        }
+
+        public void SaveFile()
+        {
+            
+        }
+    }
+}
---
+++ TurtleXmlStorage/TurtleConfigurationHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace TurtleXmlStorage
+{
+    public class TurtleConfigurationHandler : FileHandler
+    {
+        public TurtleConfigurationHandler() : base("TurtleConfig.xml")
+        {
+            
+        }
+
+        public XElement GetEmptyXml()
+        {
+            var xml = new XElement("configurations", new XElement("projects"));
+            return xml;
+        }
+
+        
+    }
+}
